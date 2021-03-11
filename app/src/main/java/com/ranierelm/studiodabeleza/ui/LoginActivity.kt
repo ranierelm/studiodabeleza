23243a1c0b56b529abc7e92bbf8223fdc1cddc7e
ranierelm <raniereprojeto@gmail.com>
2021-03-11 14:49:38 -0300

    Config details button, and themes

---
+++ app/src/main/java/com/ranierelm/studiodabeleza/ui/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.ranierelm.studiodabeleza.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ranierelm.studiodabeleza.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+//        txt_create_account.setOnClickListener(){
+//            registrationScreen()
+//        }
+//
+//        txt_forgot_password.setOnClickListener(){
+//            recoveryScreen()
+//        }
+
+
+    }
+
+//    private fun registrationScreen(){
+//
+//        val goRegister = Intent(this, RegistrationActivity::class.java )
+//        startActivity(goRegister)
+//    }
+//
+//    private fun recoveryScreen(){
+//        val recoveryPassword = Intent(this, RecoveryPasswordActivity::class.java)
+//        startActivity(recoveryPassword)
+//    }
+
+}
---
+++ app/src/main/java/com/ranierelm/studiodabeleza/ui/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.ranierelm.studiodabeleza.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.ranierelm.studiodabeleza.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        },3000)
+    }
+}
